1a5ecdc880ce284ad854a73d96465f204d85e0a4
naveedkhan888 <88194719+naveedkhan888@users.noreply.github.com>
2024-08-22 16:44:19 +0500

    sdfdsf

--- inc/frontend/widgets/recent-posts.php
+++ inc/frontend/widgets/recent-posts.php
@@ -77,13 +77,13 @@ class restimo_recent_news extends WP_Widget {
                     <?php if( $post_thumb ) { ?>
                     <div class="thumb">
                         <a href="<?php the_permalink( $recent_post->ID ); ?>">
-                            <?php echo $post_thumb; ?>
+                            <?php echo wp_kses_post( $post_thumb ); ?>
                         </a>
                     </div>
                     <?php } ?>
                     <div class="entry-header">
                         <h6>
-                            <a href="<?php the_permalink( $recent_post->ID ); ?>"><?php echo $title; ?></a>
+                            <a href='<?php echo esc_url( get_permalink( $recent_post->ID ) ); ?>'><?php echo esc_html( $title ); ?></a>
                         </h6>
                         <?php if($date) { ?>
                         <span class="post-on">
